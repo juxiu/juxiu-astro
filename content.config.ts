2ebd5ffcf3f2b421638cb8fe21014e45486ed9f2
郭林涛 <v-guolintao1@sinochem.com>
2025-01-24 17:26:05 +0800

    feat: router

---
+++ content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from 'astro/loaders';
+const postCollection = defineCollection({
+    // type: "content",
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/post" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        dateFormatted: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    }),
+});
+
+export const collections = {
+    post: postCollection,
+};
