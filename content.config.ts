35d4dfc74499395686e97d0b7486cd0a1ef862d4
juxiu <iguolintao@163.com>
2025-01-25 15:22:07 +0800

    list

--- content.config.ts
+++ content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from 'astro/loaders';
 const postCollection = defineCollection({
     // type: "content",
-    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/post" }),
+    loader: glob({ pattern: "**/*.md", base: "./src/content/post" }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
---
+++ src/collections/menu.ts
@@ -0,0 +1,18 @@
+export const menus = [
+	{
+		"name": "Home",
+		"url": "/"
+	},
+	// {
+	// 	"name": "Posts",
+	// 	"url": "/posts"
+	// },
+	// {
+	// 	"name": "Projects",
+	// 	"url": "/projects"
+	// },
+	// {
+	// 	"name": "About",
+	// 	"url": "/about"
+	// }
+]
